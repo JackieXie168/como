26aa1de5404413758d9690fe596b8d9b8f5bb125
iannak1 <iannak1@d01d2539-6c13-0410-b5bf-e19f60f28a35>
2005-09-25 20:19:56 +0000

    Fixing getprotobyname() for linux (that has strange protocol names in /etc/protocols --- e.g., ESP is IPV6-CRYPT).

--- src/trunk/base/util.c
+++ src/trunk/base/util.c
@@ -453,47 +453,48 @@ error:
  * variable name (?!?). In order to avoid the mess, it is easier
  * to rewrite the function from scratch. 
  * 
+ * Given that we are at it we also fix the problem with /etc/protocols 
+ * in Linux that gives uncommon names to common protocols (e.g., protocol 
+ * number 50 is better known as ESP than IPV6-CRYPT). 
+ * 
+ * we know the name of the first 133 protocols.
  */
-char *
+
+static char * 
+alias[256] = { 
+    "ip","icmp","igmp","ggp","ipencap","st2","tcp","cbt","egp",
+    "igp","bbn-rcc","nvp","pup","argus","emcon","xnet","chaos",
+    "udp","mux","dcn","hmp","prm","xns-idp","trunk-1","trunk-2",
+    "leaf-1","leaf-2","rdp","irtp","iso-tp4","netblt","mfe-nsp",
+    "merit-inp","sep","3pc","idpr","xtp","ddp","idpr-cmtp","tp++",
+    "il","ipv6","sdrp","ipv6-route","ipv6-frag","idrp","rsvp","gre",
+    "mhrp","bna","esp","ah","i-nlsp","swipe","narp","mobile","tlsp",
+    "skip","ipv6-icmp","ipv6-nonxt","ipv6-opts","cftp","sat-expak",
+    "kryptolan","rvd","ippc","sat-mon","visa","ipcv","cpnx","cphb",
+    "wsn","pvp","br-sat-mon","sun-nd","wb-mon","wb-expak","iso-ip",
+    "vmtp","secure-vmtp","vines","ttp","nsfnet-igp","dgp","tcf",
+    "eigrp","ospf","sprite-rpc","larp","mtp","ax.25","ipip","micp",
+    "scc-sp","etherip","encap","99","gmtp","ifmp","pnni","pim","aris",
+    "scps","qnx","a/n","ipcomp","snp","compaq-peer","ipx-in-ip",
+    "carp","pgm","l2tp","ddx","iatp","st","srp","uti","smp","sm",
+    "ptp","isis","fire","crtp","crudp","sscopmce","iplt","sps",
+    "pipe","sctp","fc","134","135","136","137","138","139","140",
+    "141","142","143","144","145","146","147","148","149","150",
+    "151","152","153","154","155","156","157","158","159","160",
+    "161","162","163","164","165","166","167","168","169","170",
+    "171","172","173","174","175","176","177","178","179","180",
+    "181","182","183","184","185","186","187","188","189","190",
+    "191","192","193","194","195","196","197","198","199","200",
+    "201","202","203","204","205","206","207","208","209","210",
+    "211","212","213","214","215","216","217","218","219","220",
+    "221","222","223","224","225","226","227","228","229","230",
+    "231","232","233","234","235","236","237","238","239","pfsync",
+    "241","242","243","244","245","246","247","248","249","250",
+    "251","252","253","254","255"};
+
+__inline__ char *
 getprotoname(int proto) 
 {
-    static char * alias[256] = {0}; 
-    int i; 
-
-    /* if alias array is not initialize, open /etc/protocols to 
-     * read the protocol names.
-     */
-    if (alias[0] == NULL) { 
-	FILE * fp; 
-	char line[120];
-
-	/* 
-	 * set all protocol aliases to the number 
- 	 */
-	for (i = 0; i < 256; i++) 
-	    asprintf(&alias[i], "%d", i); 
-
-	/* open /etc/protocols and read all defined names */
-	fp = fopen("/etc/protocols", "r"); 
-	if (fp == NULL) 
-	    panic("opening /etc/protocols"); 
-	
-	while (!feof(fp)) { 
-	    char name[128]; 
-	    char * wh; 
-	    int number; 
-
-	    fgets(line, 120, fp); 
-	    wh = index(line, '#'); 
-	    if (wh != NULL) 
-		*wh = 0; 		/* remove comments */
-
-	    if (sscanf(line, "%s %d", name, &number) == 2) 
-		if (number < 256) 
-		    alias[number] = strdup(name); 
-	} 
-    } 
-
     return alias[proto]; 
 }
 	    
