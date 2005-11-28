ae3fe74f9be740111e01f119202f499fbe1bb119
iannak1 <iannak1@d01d2539-6c13-0410-b5bf-e19f60f28a35>
2005-11-28 23:57:22 +0000

    Modified module to count the cumulative bytes for source and destination ports and find the top N with just one pass of the array (i.e., without export).
    The HTML format prints port numbers and Mbps only.

--- src/trunk/modules/topports.c
+++ src/trunk/modules/topports.c
@@ -27,10 +27,10 @@
  */
 
 /*
- * Top-N port pairs module.
+ * Top-N ports module.
  *
- * This module computes the top-N port pairs with more bytes transmitted
- * between them.
+ * This module finds the top N port numbers (source or destination) with the 
+ * largest number of bytes sent during a given interval. 
  *
  */
 
@@ -39,27 +39,16 @@
 #include "como.h"
 #include "module.h"
 
-#define TOPN        20		/* top-20 port pairs by default */
-#define GRANULARITY  5		/* default measurement granularity (secs) */
-
 #define FLOWDESC	struct _topports
-#define EFLOWDESC	FLOWDESC
 
 FLOWDESC {
-    uint32_t ts;	/* timestamp of first packet */
-    n16_t sport;        /* source port number */
-    n16_t dport;        /* destination port number */
-    uint64_t bytes;	/* number of bytes */
-    uint64_t pkts;	/* number of packets */
+    uint32_t ts;			/* timestamp of first packet */
+    uint64_t bytes[65536];		/* bytes per port number */
+    uint64_t pkts[65536];		/* pkts per port number */
 };
 
-/* 
- * static variable for the modules. 
- * XXX we should get rid of these to force callbacks to be closures. 
- */
-static unsigned int granularity = GRANULARITY;   /* measurement
-                                                  * granularity (secs) */
-static int topn = TOPN;    /* number of top port pairs */
+static timestamp_t meas_ivl = TIME2TS(1, 0);	/* interval (secs) */
+static int topn = 20;    			/* number of top ports */
 
 static timestamp_t
 init(__unused void *mem, __unused size_t msize, char *args[])
@@ -67,16 +56,13 @@ init(__unused void *mem, __unused size_t msize, char *args[])
     int i;
     char *len;
     
-    granularity = GRANULARITY;  
-    topn = TOPN;    
-
     /* 
      * process input arguments 
      */
     for (i = 0; args && args[i]; i++) { 
-	if (strstr(args[i], "granularity")) {
+	if (strstr(args[i], "interval")) {
 	    len = index(args[i], '=') + 1; 
-	    granularity = atoi(len); 
+	    meas_ivl = TIME2TS(atoi(len), 0); 
 	} 
 	if (strstr(args[i], "topn")) {
 	    len = index(args[i], '=') + 1;
@@ -84,188 +70,132 @@ init(__unused void *mem, __unused size_t msize, char *args[])
 	}
     }
 
-    return TIME2TS(granularity, 0);
+    return meas_ivl; 
 }
 
 static int
 check(pkt_t *pkt)
 {
-    /* accept only TCP or UDP packets */
-    return (isTCP || isUDP);
+    return (isTCP || isUDP); 	/* accept only TCP or UDP packets */
 }
 
-static uint32_t
-hash(pkt_t *pkt)
-{
-    /* hash by source port */
-    if (IP(proto) == IPPROTO_TCP)
-	return N16(TCP(src_port));
-    else /* UDP */
-	return N16(UDP(src_port));
-}
 
 static int
-match(pkt_t *pkt, void *fh)
+update(pkt_t *pkt, void *rp, int isnew)
 {
-    FLOWDESC *x = F(fh);
-    
-    /* match by destination port */
-    if (IP(proto) == IPPROTO_TCP)
-        return (N16(TCP(dst_port)) == N16(x->dport));
-    else /* UDP */
-        return (N16(UDP(dst_port)) == N16(x->dport));
-}
-
-static int
-update(pkt_t *pkt, void *fh, int isnew)
-{
-    FLOWDESC *x = F(fh);
+    FLOWDESC *x = F(rp);
 
     if (isnew) {
-	x->ts = TS2SEC(pkt->ts) - (TS2SEC(pkt->ts) % granularity);
-        if (IP(proto) == IPPROTO_TCP) {
-            x->sport = TCP(src_port);
-            x->dport = TCP(dst_port);
-        } else /* UDP */ {
-            x->sport = UDP(src_port);
-            x->dport = UDP(dst_port);
-        }
-        x->bytes = 0;
-        x->pkts = 0;
+	bzero(x, sizeof(FLOWDESC)); 
+	x->ts = TS2SEC(pkt->ts - pkt->ts % meas_ivl);
     }
 
-    x->bytes += H16(IP(len));
-    x->pkts++;
+    if (isTCP) {
+	x->bytes[H16(TCP(src_port))] += H16(IP(len));
+	x->bytes[H16(TCP(dst_port))] += H16(IP(len));
+	x->pkts[H16(TCP(src_port))]++;
+	x->pkts[H16(TCP(dst_port))]++;
+    } else { 
+	x->bytes[H16(UDP(src_port))] += H16(IP(len));
+	x->bytes[H16(UDP(dst_port))] += H16(IP(len));
+	x->pkts[H16(UDP(src_port))]++;
+	x->pkts[H16(UDP(dst_port))]++;
+    } 
 
     return 0;
 }
 
-static int
-ematch(void *efh, void *fh)
-{
-    FLOWDESC *x = F(fh);
-    EFLOWDESC *ex = EF(efh);
 
-    return (N16(x->dport) == N16(ex->dport));
-}
+struct topports {
+    uint32_t ts;		/* timestamp of first packet */
+    uint32_t port; 		/* port number */
+    uint64_t bytes; 		/* bytes/port number */
+    uint64_t pkts;		/* pkts/port number */
+};
 
-static int
-export(void *efh, void *fh, int isnew)
-{
-    FLOWDESC *x = F(fh);
-    EFLOWDESC *ex = EF(efh);
-
-    if (isnew) {
-	ex->ts = x->ts; 
-        ex->sport = x->sport;
-        ex->dport = x->dport;
-        ex->bytes = 0;
-        ex->pkts = 0;
-    }
-
-    ex->bytes += x->bytes;
-    ex->pkts += x->pkts;
-
-    return 0;
-}
-
-static int
-compare(const void *efh1, const void *efh2)
-{
-    EFLOWDESC *ex1 = CMPEF(efh1);
-    EFLOWDESC *ex2 = CMPEF(efh2);
-
-    return ((ex1->bytes > ex2->bytes)? -1 : 1);
-}
-
-static int
-action(void *efh, timestamp_t current_time, int count)
-{
-    static uint32_t last_export = 0; 
-
-    if (efh == NULL) { 
-	/* 
-	 * this is the action for the entire table. 
-	 * check if it is time to export the table. 
-	 * if not stop. 
-	 */
-	uint32_t ivl = TS2SEC(current_time) -
-                       TS2SEC(current_time) % granularity;
-	if (ivl - last_export < granularity) 
-	    return ACT_STOP;		/* too early */
-
-	last_export = ivl; 
-	return ACT_GO; 		/* dump the records */
-    }
-
-    return (count < topn)? ACT_STORE|ACT_DISCARD : ACT_DISCARD; 
-}
 
 static ssize_t
-store(void *efh, char *buf, size_t len)
+store(void *rp, char *buf, size_t len)
 {
-    EFLOWDESC *ex = EF(efh);
+    FLOWDESC *x = F(rp);
+    int top_ports[65536 + 1];		/* we need one more the max topN */
+    int i, j;
     
-    if (len < sizeof(EFLOWDESC))
+    if (len < topn * sizeof(struct topports))
         return -1;
 
-    PUTH32(buf, ex->ts);
-    PUTN16(buf, N16(ex->sport));
-    PUTN16(buf, N16(ex->dport));
-    PUTH64(buf, ex->bytes);
-    PUTH64(buf, ex->pkts);
+    /* go thru the array of bytes to find the topn port 
+     * numbers. store them in the top_ports array. 
+     */
+    bzero(top_ports, sizeof(top_ports));
+    for (i = 0; i < 65536; i++) { 
+	if (x->bytes[i] == 0)
+	    continue;
 
-    return sizeof(EFLOWDESC);
+	for (j = topn - 1; j >= 0; j--) {
+	    if (x->bytes[i] < x->bytes[top_ports[j]])
+		break;
+
+	    top_ports[j + 1] = top_ports[j];
+	    top_ports[j] = i;
+	}
+    }
+
+    /* 
+     * save the first top N entries 
+     */
+    for (i = 0; i < topn && x->bytes[top_ports[i]] > 0; i++) { 
+	PUTH32(buf, x->ts);
+	PUTH32(buf, top_ports[i]); 
+	PUTH64(buf, x->bytes[top_ports[i]]);
+	PUTH64(buf, x->pkts[top_ports[i]]);
+    } 
+
+    return i * sizeof(struct topports);
 }
 
 static size_t
 load(char *buf, size_t len, timestamp_t *ts)
 {
-    if (len < sizeof(EFLOWDESC)) {
+    if (len < sizeof(struct topports)) {
         ts = 0;
         return 0; 
     }
 
-    *ts = TIME2TS(ntohl(((EFLOWDESC *)buf)->ts), 0);
-    return sizeof(EFLOWDESC);
+    *ts = TIME2TS(ntohl(((struct topports *)buf)->ts), 0);
+    return sizeof(struct topports);
 }
 
 
 #define PRETTYHDR	\
-    "Date                     Sport Dport       Bytes       Packets   \n"
+    "Date                     Port  Bytes       Packets   \n"
 
-#define PRETTYFMT 	"%.24s %5u %5u %10llu %10llu\n"
+#define PRETTYFMT 	"%.24s %5u %10llu %10llu\n"
 
-#define PLAINFMT	"%12u %5u %5u %10llu %10llu\n"
+#define PLAINFMT	"%12u %5u %10llu %10llu\n"
 
 #define HTMLHDR							\
     "<html><body>\n"						\
     "<table cellpadding=1>\n"					\
     "  <tr>\n"							\
     "    <td width=200 style=\"border-bottom:1px solid\">\n"	\
-    "      <b>Sport</b></td>\n"			                \
-    "    <td width=200 style=\"border-bottom:1px solid\">\n"	\
-    "      <b>Dport</b></td>\n"			                \
+    "      <b>Port</b></td>\n"			                \
     "    <td width=150 style=\"border-bottom:1px solid\">\n"	\
-    "      <b>Bytes</b></td>\n"					\
-    "    <td width=150 style=\"border-bottom:1px solid\">\n"	\
-    "      <b>Packets</b></td>\n"				\
+    "      <b>Mbps</b></td>\n"					\
     "  </tr>\n"						
 
 #define HTMLFOOTER						\
     "</table>\n"						\
     "</body></html>\n"						
 
-#define HTMLFMT		"<tr><td>%5u</td><td>%5u</td><td>%10llu</td>" \
-                        "<td>%10llu</td></tr>\n"
+#define HTMLFMT		"<tr><td>%5u</td><td>%.2f</td></tr>\n"
 
 static char *
 print(char *buf, size_t *len, char * const args[])
 {
     static char s[2048];
     static char * fmt; 
-    EFLOWDESC *x; 
-    uint16_t sport, dport;
+    struct topports *x; 
     time_t ts;
 
     if (buf == NULL && args != NULL) { 
@@ -297,18 +227,16 @@ print(char *buf, size_t *len, char * const args[])
   	return s; 
     } 
 
-    x = (EFLOWDESC *) buf;
+    x = (struct topports *) buf;
     ts = (time_t) ntohl(x->ts);
-    sport = H16(x->sport);
-    dport = H16(x->dport);
     if (fmt == PRETTYFMT) { 
-	*len = sprintf(s, fmt, asctime(localtime(&ts)), sport, dport,
+	*len = sprintf(s, fmt, asctime(localtime(&ts)), ntohl(x->port),
 		   NTOHLL(x->bytes), NTOHLL(x->pkts));
     } else if (fmt == HTMLFMT) { 
-	*len = sprintf(s, fmt, sport, dport,
-		   NTOHLL(x->bytes), NTOHLL(x->pkts));
+	float mbps = (float) NTOHLL(x->bytes) * 8 / (float) TS2SEC(meas_ivl);
+	*len = sprintf(s, fmt, ntohl(x->port), mbps); 
     } else { 
-	*len = sprintf(s, fmt, ts, sport, dport,
+	*len = sprintf(s, fmt, ts, ntohl(x->port), 
 		   NTOHLL(x->bytes), NTOHLL(x->pkts));
     } 
 	
@@ -318,19 +246,19 @@ print(char *buf, size_t *len, char * const args[])
 
 callbacks_t callbacks = {
     ca_recordsize: sizeof(FLOWDESC),
-    ex_recordsize: sizeof(EFLOWDESC),
-    st_recordsize: sizeof(EFLOWDESC),
+    ex_recordsize: 0, 
+    st_recordsize: sizeof(FLOWDESC),
     indesc: NULL, 
     outdesc: NULL, 
     init: init,
     check: check,
-    hash: hash,
-    match: match,
+    hash: NULL,
+    match: NULL,
     update: update,
-    ematch: ematch,
-    export: export,
-    compare: compare,
-    action: action,
+    ematch: NULL,
+    export: NULL,
+    compare: NULL,
+    action: NULL,
     store: store,
     load: load,
     print: print,
