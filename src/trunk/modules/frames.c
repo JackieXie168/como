e570d79b358f9fa9a78453bc5e5ecb276469c1ae
kpmcgrath <kpmcgrath@d01d2539-6c13-0410-b5bf-e19f60f28a35>
2006-01-31 21:37:25 +0000

    . minor edits, to report traffic volume per second

--- src/trunk/modules/frames.c
+++ src/trunk/modules/frames.c
@@ -27,10 +27,8 @@
  */
 
 /*
- * 802.11 Frame Breakdown Report
+ * 802.11 frame statistics report 
  *
- * The sniffer reports statistics about frame types during the 
- * measurement duration 
  */
 
 #include <stdio.h>
@@ -42,9 +40,14 @@ static int meas_ivl = 1; /* measurement interval */
 #define FLOWDESC    struct _frames
 FLOWDESC {
     timestamp_t ts;
+
     uint64_t    mgmtpkts;
     uint64_t    ctrlpkts;
     uint64_t    datapkts;
+
+    uint64_t    mgmtbytes;
+    uint64_t    ctrlbytes;
+    uint64_t    databytes;
 };
 
 
@@ -56,17 +59,21 @@ update(pkt_t *pkt, void *fh, int isnew)
     if (isnew) {
 	x->ts = pkt->ts;
         x->mgmtpkts = x->ctrlpkts = x->datapkts = 0;
+        x->mgmtbytes = x->ctrlbytes = x->databytes = 0;
     }
 
     switch(WLANTYPE(COMO(l2type))) {
     case WLANTYPE_MGMT:
 	x->mgmtpkts++;
+        x->mgmtbytes += COMO(len);
         break;
     case WLANTYPE_CTRL:
 	x->ctrlpkts++;
+        x->ctrlbytes += COMO(len);
 	break;
     case WLANTYPE_DATA: 
 	x->datapkts++;
+        x->databytes += COMO(len);
 	break;
     default:
 	break;
@@ -88,6 +95,10 @@ store(void *rp, char *buf, size_t len)
     PUTH64(buf, x->ctrlpkts);
     PUTH64(buf, x->datapkts);
 
+    PUTH64(buf, x->mgmtbytes);
+    PUTH64(buf, x->ctrlbytes);
+    PUTH64(buf, x->databytes);
+
     return sizeof(FLOWDESC);
 }
 
@@ -104,16 +115,21 @@ load(char * buf, size_t len, timestamp_t * ts)
 }
 
 
-#define PRETTYHDR		\
-    "Timestamp           Management frames    Control frames       Data frames\n"
-#define PRETTYFMT	"%12d.%06d %-20llu %-20llu %-20llu\n"
-#define PLAINFMT	"%12ld %16llu %12llu %12llu %12llu\n"
-#define GNUPLOTFMT	"%ld %llu %llu %llu\n"
+#define PRETTYHDR			\
+    "date                       "	\
+    "timestamp         "		\
+    "mgmt (frames, bytes)       "	\
+    "ctrl (frames, bytes)       "	\
+    "data (frames, bytes)\n"
+
+#define GNUPLOTFMT  "%ld %llu %llu %llu %llu %llu %llu %llu %llu %llu\n"
+#define PRETTYFMT 		\
+    "%.24s %12d.%06d %-5llu %-20llu %-5llu %-20llu %-5llu %-20llu\n"
 
 #define GNUPLOTHDR						\
     "set terminal postscript eps color solid lw 1 \"Helvetica\" 14;"	\
-    "set grid;"								\
-    "set ylabel \"Frame Count\" textcolor lt 3;"			\
+    "set grid;"                                                         \
+    "set ylabel \"Bytes per Second\";"	        	                \
     "set xlabel \"Time (HH:MM UTC)\";"					\
     "set yrange [0:*];"							\
     "set autoscale xfix;"						\
@@ -121,9 +137,9 @@ load(char * buf, size_t len, timestamp_t * ts)
     "set xdata time;"							\
     "set timefmt \"%%s\";"						\
     "set format x \"%%H:%%M\";"						\
-    "plot \"-\" using 1:2 with filledcurve x1 title \"MGMT\" lw 5," \
-    "     \"-\" using 1:3 with filledcurve x1 title \"CTRL\" lw 5," \
-    "     \"-\" using 1:4 with filledcurve x1 title \"DATA\" lw 5;\n"
+    "plot \"-\" using 1:3 with filledcurve x1 title \"MGMT\" lw 5," \
+    "     \"-\" using 1:5 with filledcurve x1 title \"CTRL\" lw 5," \
+    "     \"-\" using 1:7 with filledcurve x1 title \"DATA\" lw 5;\n"
 
 #define GNUPLOTFOOTER	"e\n"
 
@@ -133,7 +149,6 @@ print(char *buf, size_t *len, char * const args[])
     static char s[512];
     static char * fmt; 
     static int granularity = 1; 
-    static int no_records = 0; 
     FLOWDESC *x; 
     timestamp_t ts;
     time_t t; 
@@ -145,26 +160,21 @@ print(char *buf, size_t *len, char * const args[])
 	fmt = PRETTYFMT; 
 
 	/* first call of print, process the arguments and return */
-	for (n = 0; args[n]; n++) {
-	    if (!strcmp(args[n], "format=plain")) {
-		*len = 0; 
-		fmt = PLAINFMT;
-	    } 
+        for (n = 0; args[n]; n++) {
 	    if (!strcmp(args[n], "format=gnuplot")) {
-		*len = sprintf(s, GNUPLOTHDR); 
-		fmt = GNUPLOTFMT;
-	    } 
-	    if (!strncmp(args[n], "granularity=", 10)) {
-		char * val = index(args[n], '=') + 1;
+                *len = sprintf(s, GNUPLOTHDR);
+                fmt = GNUPLOTFMT;
+            } else if (!strncmp(args[n], "granularity=", 10)) {
+                char * val = index(args[n], '=') + 1;
 
-		/* aggregate multiple records into one to reduce 
-		 * communication messages. 
-		 */
-		granularity = MAX(atoi(val) / meas_ivl, 1);
-	    } 
+                /* aggregate multiple records into one to reduce
+                 * communication messages.
+                 */
+                granularity = MAX(atoi(val) / meas_ivl, 1);
+            }
 	} 
-	return s; 
-    } 
+	return s;	 
+    }
 
     if (buf == NULL && args == NULL) { 
 	*len = 0;
@@ -176,23 +186,17 @@ print(char *buf, size_t *len, char * const args[])
     x = (FLOWDESC *) buf; 
     ts = NTOHLL(x->ts);
     t = (time_t) TS2SEC(ts); 
-    no_records++;
-    
-    if(no_records %  granularity != 0) {
-	*len = 0;
-	return s;
-    }
+
     /* print according to the requested format */
     if (fmt == PRETTYFMT) {
-	*len = sprintf(s, fmt, TS2SEC(ts), TS2USEC(ts), NTOHLL(x->mgmtpkts), 
-				    NTOHLL(x->ctrlpkts), NTOHLL(x->datapkts)); 
-    }
-      else if (fmt == GNUPLOTFMT) {
+	*len = sprintf(s, fmt, asctime(localtime(&t)), TS2SEC(ts), TS2USEC(ts),
+		NTOHLL(x->mgmtpkts), NTOHLL(x->mgmtbytes), NTOHLL(x->ctrlpkts),
+		NTOHLL(x->ctrlbytes), NTOHLL(x->datapkts), 
+		NTOHLL(x->databytes)); 
+    } else if (fmt == GNUPLOTFMT) {
 	*len = sprintf(s, fmt, (long int)t, NTOHLL(x->mgmtpkts), 
-				    NTOHLL(x->ctrlpkts), NTOHLL(x->datapkts)); 
-    } else {
-	*len = sprintf(s, fmt, (long int)t, ts, NTOHLL(x->mgmtpkts), 
-				    NTOHLL(x->ctrlpkts), NTOHLL(x->datapkts)); 
+		NTOHLL(x->ctrlpkts), NTOHLL(x->datapkts), NTOHLL(x->mgmtbytes), 
+		NTOHLL(x->ctrlbytes), NTOHLL(x->databytes)); 
     } 
 
     return s;
@@ -201,7 +205,7 @@ print(char *buf, size_t *len, char * const args[])
 callbacks_t callbacks = {
     ca_recordsize: sizeof(FLOWDESC),
     ex_recordsize: 0,
-    st_recordsize: 65535,
+    st_recordsize: sizeof(FLOWDESC),
     indesc: NULL, 
     outdesc: NULL,
     init: NULL,
@@ -217,5 +221,5 @@ callbacks_t callbacks = {
     load: load,
     print: print,
     replay: NULL,
-    formats: "plain pretty gnuplot"
+    formats: "pretty gnuplot"
 };
