00644e02538ae74c734e84d0ebf974881c3572d5
m_canini <m_canini@d01d2539-6c13-0410-b5bf-e19f60f28a35>
2006-02-14 15:22:03 +0000

    Fixed trivial typo.

--- src/trunk/modules/trace.c
+++ src/trunk/modules/trace.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2004 Intel Corporation
- * All r ghts reserved.
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
