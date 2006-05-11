0cd0cbc3cf056b353290a2d4996325bdc5fff7f2
m_canini <m_canini@d01d2539-6c13-0410-b5bf-e19f60f28a35>
2006-05-11 09:05:45 +0000

    Updating Id.

--- src/trunk/base/status.c
+++ src/trunk/base/status.c
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: query.c 452 2006-05-10 14:07:06Z m_canini $
+ * $Id$
  */
 
 #include <unistd.h>
