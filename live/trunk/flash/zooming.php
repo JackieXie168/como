5153be2bdf55604703c120b8b30ae027be7b8f9d
rgass <rgass@d01d2539-6c13-0410-b5bf-e19f60f28a35>
2006-10-09 11:09:12 +0000

    adding urlencode to the url

--- live/trunk/flash/zooming.php
+++ live/trunk/flash/zooming.php
@@ -15,24 +15,25 @@
     $info .= "&cursor=../flash/global_cursor.swf";
 
     /* the query that the flash client needs to send back */
-    $info .= "&link=dashboard.php&target=_top";
+    $info .= "&link=dashboard.php&target=_TOP";
     $info .= "&comonode=$comonode&module=$module";
     $info .= "&start=$start&end=$end&format=gnuplot&";
     $info .= "$http_query_string";
+    $info = urlencode($info);
 ?>
 
 <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" 
         codebase="http://macromedia.com/cabs/flash/swflash.cab#version=7,0,0,0" 
         width="600" height="450" id="zooming" align="middle">
     <param name="allowScriptAccess" value="sameDomain">
-    <param name="movie" value="flash/zooming.swf?<?php echo($info);?>">
+    <param name="movie" value="../flash/zooming.swf?<?php echo($info);?>">
     <param name="loop" value="false">
     <param name="menu" value="false">
     <param name="quality" value="medium">
     <param name="salign" value="r">
     <param name="wmode" value="opaque">
     <param name="bgcolor" value="#ffffff">
-    <embed src="flash/zooming.swf?<?php echo($info);?>"
+    <embed src="../flash/zooming.swf?<?php echo($info);?>"
 	   loop="false" menu="false" quality="medium"  salign="r"  
            wmode="opaque" bgcolor="#ffffff" width="600" height="450" 
            name="zoom" align="middle" allowScriptAccess="sameDomain" 
