eb16d37bd20ea1bfd5a95610a3ff39a786d1e68b
jsanjuas <jsanjuas@d01d2539-6c13-0410-b5bf-e19f60f28a35>
2006-10-19 12:10:30 +0000

    - Created a lib entry for Boyer-Moore pattern searching - Created new module pattern_search - Cosmetics in trace module and sorted module list in cmakelists

---
+++ src/trunk/lib/pattern_search.c
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) 2006 Intel Corporation
+ * Copyright (c) 2005 Universitat Politecnica de Catalunya 
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id$
+ */
+
+#include "como.h"
+#include "pattern_search.h"
+
+/*
+ * -- preBmBc
+ *  
+ * Precalculate the first jump table
+ *
+ */
+static void
+preBmBc(char *x, int m, int bmBc[])
+{
+    int i;
+ 
+    for (i = 0; i < ASIZE; ++i)
+        bmBc[i] = m;
+    for (i = 0; i < m - 1; ++i)
+        bmBc[(unsigned char)x[i]] = m - i - 1;
+}
+ 
+/*
+ * -- suffixes 
+ *  
+ * Calculate all the terminal substrings of a
+ * given string
+ *
+ */
+static void
+suffixes(char *x, int m, int *suff)
+{
+    int f, g, i;
+ 
+    suff[m - 1] = m;
+    g = m - 1;
+    for (i = m - 2; i >= 0; --i) {
+        if (i > g && suff[i + m - 1 - f] < i - g)
+            suff[i] = suff[i + m - 1 - f];
+        else {
+            if (i < g)
+                g = i;
+            f = i;
+            while (g >= 0 && x[g] == x[g + m - 1 - f])
+                --g;
+            suff[i] = f - g;
+        }
+    }
+}
+ 
+/*
+ * -- preBmGs
+ *  
+ * Precalculate the second jump table
+ *
+ */
+static void
+preBmGs(char *x, int m, int bmGs[])
+{
+    int i, j, suff[m];
+ 
+    suffixes(x, m, suff);
+ 
+    for (i = 0; i < m; ++i)
+        bmGs[i] = m;
+    j = 0;
+    for (i = m - 1; i >= -1; --i)
+        if (i == -1 || suff[i] == i + 1)
+            for (; j < m - 1 - i; ++j)
+                if (bmGs[j] == m)
+                    bmGs[j] = m - 1 - i;
+    for (i = 0; i <= m - 2; ++i)
+        bmGs[m - 1 - suff[i]] = m - 1 - i;
+}
+ 
+
+/*
+ * -- BM()
+ *
+ * Boyer-Moore pattern search
+ * 
+ * Given a string, a pattern to search, their respective sizes, 
+ * and precomputed data, this function returns the first appearance 
+ * of the pattern in the string (or 0 if the pattern is not found).
+ *
+ */
+static int 
+BM(char *x, int m, char *y, int n, int bmBc[], int bmGs[], uint *found) 
+{
+    int i, j, k;
+   
+    *found = 0;
+
+    j = 0;
+    while (j <= n - m) {
+	/* 
+	 * Compare the pattern and the window backwards
+	 * and starting from their rightmost position 
+	 */
+	for (i = m - 1; i >= 0 && x[i] == y[i + j]; --i);
+      
+	if (i < 0) { /* found! */
+	    *found = j;
+	    return 1; 
+	} 
+
+	/* 
+	 * The pattern was not found. The following shift
+	 * will be the maximum between the shifts in the 
+	 * two precomputed tables
+	 */
+	k = bmBc[(unsigned char) y[i + j]] - m + 1 + i; 
+	j += (bmGs[i] > k)? bmGs[i] : k; 
+    }
+
+    return 0;
+}
+
+void
+pattern_search_initialize(pattern_search_t *ps, char *pattern)
+{
+    memset(ps, 0, sizeof(pattern_search_t));
+#define __min(a,b) (a) < (b) ? (a) : (b)
+    ps->patsize = __min(strlen(pattern), MAX_PATTERN_SIZE);
+#undef __min
+    strncpy(ps->pattern, pattern, ps->patsize);
+
+    /* precompute the jump tables */
+    preBmBc(ps->pattern, ps->patsize, ps->bmBc);
+    preBmGs(ps->pattern, ps->patsize, ps->bmGs);
+}
+
+int
+pattern_search(pattern_search_t *ps, char *string, size_t len, int *where)
+{
+    int discard, found;
+
+    found = BM(ps->pattern, ps->patsize, string, len,
+                ps->bmBc, ps->bmGs, where ? where : &discard);
+
+    return found;
+}
+
