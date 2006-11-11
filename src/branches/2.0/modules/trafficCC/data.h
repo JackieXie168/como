315746a8a6e98db4bd5e5e4770b966f541d11683
jsanjuas <jsanjuas@d01d2539-6c13-0410-b5bf-e19f60f28a35>
2006-11-11 12:39:10 +0000

    Advancements towards having a build system for modules. Added dirs for two modules, trafficCC and trafficCCS, which will become an example of modules written in C / C + C#. Fixed some copyright notices in structmagic stuff.

---
+++ src/branches/2.0/modules/trafficCC/data.h
@@ -0,0 +1,15 @@
+#include "como.h"
+
+como_record struct traffic_record {
+    timestamp_t ts;
+    uint64_t    bytes[2];
+    uint32_t    pkts[2];
+};
+
+como_config struct traffic_config {
+    int meas_ivl;	/* measurement interval */
+    int iface;		/* interface */
+};
+
+//typedef struct config config_t;
+
