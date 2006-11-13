7ffdd92f7a034b43bf914997b8773baa7ab3378d
m_canini <m_canini@d01d2539-6c13-0410-b5bf-e19f60f28a35>
2006-11-13 19:28:48 +0000

    Towards running a module!

--- src/branches/2.0/include/mdl.h
+++ src/branches/2.0/include/mdl.h
@@ -3,6 +3,35 @@
 
 typedef struct mdl mdl_t;
 
+/*
+ * Module callbacks
+ */
+
+
+/**
+ * su_init_fn() run from supervisor to initialize a module,
+ * For the time being, just initialize the private memory for the module,
+ * and take arguments from the config file.
+ * Returns the config state.
+ */
+typedef void * (*su_init_fn) (mdl_t * h);
+
+
+/**
+ * flush_fn() run from capture at every flush interval to obtain a clean
+ * flush state.
+ */
+typedef void * (*flush_fn)   (mdl_t * h, timestamp_t ts);
+
+/**
+ * captures_fn() run from capture to update *state with the info from *pkt.
+ * state points to current flush state.
+ * Normally returns COMO_OK to continue to process the packets, can return
+ * COMO_FLUSH to force a flush.
+ */
+typedef int    (*capture_fn) (mdl_t * h, pkt_t * pkt, void * state);
+
+
 struct mdl {
     /* public fields */
     timestamp_t	flush_ivl;
@@ -13,6 +42,20 @@ struct mdl {
     /* private state */
     void *	priv;
 };
+
+#define mdl_get_config(h, type) \
+((const type *) (h->config))
+
+#define mdl_alloc_config(h, type) \
+((type *) mdl__alloc_config(h, sizeof(type), &(type ## _serializable)))
+
+#define mdl_alloc_tuple(h, type) \
+((type *) mdl__alloc_tuple(h, sizeof(type)))
+
+
+void * mdl__alloc_tuple(mdl_t * h, size_t sz);
+char * mdl_alloc_string(mdl_t * h, size_t sz);
+
 
 uint8_t * mdl_serialize   (uint8_t * sbuf, const mdl_t * h);
 size_t    mdl_expose_len  (const mdl_t * src);
